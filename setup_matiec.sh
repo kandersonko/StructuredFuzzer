87c58de55ae3341da1df90574e6b63f21e3e76f3
Koffi Anderson Koffi <kandersonko@gmail.com>
2024-07-10 09:21:48 -0600

    Update setup_matiec.sh

--- setup_matiec.sh
+++ setup_matiec.sh
@@ -11,7 +11,8 @@ autoreconf -i
 ./configure; make
 
 # Matiec compiler path
-matiec_base_path=$HOME/projects/plc_runtime_fuzzer
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+matiec_base_path="$script_dir"
 export MATIEC_INCLUDE_PATH=${matiec_base_path}/matiec/lib
 export MATIEC_C_INCLUDE_PATH=${matiec_base_path}/matiec/lib/C
 export PATH=${matiec_base_path}/matiec:$PATH
